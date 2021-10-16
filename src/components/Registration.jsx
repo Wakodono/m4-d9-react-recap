db37d3e7a58af771feb3cc9b1baddcf8c4824fd8
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-10-16 16:52:26 +0100

    Update Registration.jsx

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -14,29 +14,20 @@ const Registration = () => (
     </Form.Group>
   </Row>
 
-    <Row className="mb-3">
-      <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Group className="mb-3" controlId="formGridEmail">
         <Form.Label>Email</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
   
-      <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Group className="mb-3" controlId="formGridPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-    </Row>
 
-  <Form.Group className="mb-3" controlId="formGridAddress1">
-    <Form.Label>Address</Form.Label>
-    <Form.Control placeholder="1234 Main St" />
-  </Form.Group>
-
-  <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridCity">
-      <Form.Label>City</Form.Label>
-      <Form.Control />
-    </Form.Group>
-  </Row>
+      <Form.Group className="mb-3" controlId="formGridPasswordConfirmation">
+        <Form.Label>Password Confirmation</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
 
   <Button variant="primary" type="submit">
     Submit
